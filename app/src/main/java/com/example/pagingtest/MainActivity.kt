1f5616c3bdb34d4baf81600b803224b503bfafa2
chaminzi <milkcookiezip@gmail.com>
2022-01-10 18:21:25 +0900

    temp commit

--- app/src/main/java/com/example/pagingtest/MainActivity.kt
+++ app/src/main/java/com/example/pagingtest/MainActivity.kt
@@ -2,10 +2,45 @@ package com.example.pagingtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.widget.ArrayAdapter
+import androidx.appcompat.widget.Toolbar
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.example.pagingtest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_container) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        setSupportActionBar(binding.mainToolbar)
+
+        binding.mainToolbar.setupWithNavController(
+            navController,
+            appBarConfiguration
+        )
+
+        // SearchView 탐색
+        val items = arrayOf("부산", "울산", "서울", "대전")
+        val autoCompleteAdapter = ArrayAdapter(this, R.layout.item_auto_complete, items)
+        binding.mainAutoCompleteTv.apply {
+            setAdapter(autoCompleteAdapter)
+            setOnFocusChangeListener { view, boolean ->  }
+        }
+
     }
 }
